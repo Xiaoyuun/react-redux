468e83bca939cb1657400d6f0176fb0a2117e8bc
Julia Ruan <juliaruan2004@gmail.com>
2024-07-25 20:43:45 -0400

    inital commit

--- FriendsScreen.js
+++ FriendsScreen.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { addFriend } from './FriendsActions';
 
 class FriendsScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Add friends here!</Text>
+
+        {
+          this.props.friends.possible.map((friend, index) => (
+            <Button
+              key={friend}
+              title={`Add ${friend}`}
+              onPress={() =>
+                this.props.addFriend(index)
+              }
+            />
+          ))
+        }
 
         <Button
           title="Back to home"
@@ -27,4 +42,16 @@ const styles = StyleSheet.create({
   },
 });
 
-export default FriendsScreen;
+const mapStateToProps = (state) => {
+  const { friends } = state
+  return { friends }
+};
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    addFriend,
+  }, dispatch)
+);
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(FriendsScreen);
